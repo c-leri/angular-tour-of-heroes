c25b716a41dc29c6c7ef0b3896b7b16c8524f002
celeri <celestin.bouchet@gmail.com>
2023-02-07 22:37:31 +0100

    Started redoing the ui with tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  // current route, to know wich button to set as "current" in the navbar
+  route = '';
+  // flag to know if the mobile navigation menu is open
+  navOpen = false;
+
+  constructor(location: Location, router: Router) {
+    router.events.subscribe(() => {
+      this.route = location.path();
+    });
+  }
+
+  toggleNav() {
+    this.navOpen = !this.navOpen;
+  }
 }
