a84d58ba5454de61dab30126a0e9e72b3df76f69
celeri <celestin.bouchet@gmail.com>
2023-02-09 21:25:53 +0100

    More UI redoing with tailwind

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -8,7 +8,6 @@ import { HeroService } from "../hero.service";
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero | undefined;
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,7 +6,6 @@ import { HeroService } from "../hero.service";
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
