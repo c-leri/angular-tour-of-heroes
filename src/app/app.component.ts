0f99167e9bcafab731aa8d0732e5822a82a4def2
celeri <celestin.bouchet@gmail.com>
2023-02-11 20:12:16 +0100

    Better messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
+import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faAngular } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  faBars = faBars;
+  faXmark = faXmark;
+  faAngular = faAngular;
   title = 'Tour of Heroes';
   // current route, to know wich button to set as "current" in the navbar
   route = '';
